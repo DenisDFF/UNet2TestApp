34e0aa2b0c2461e54de24270c3dd0aafa411bd28
Denis <denis.petrov56245@gmail.com>
2025-01-03 03:51:56 +0100

    new data logic

--- data.py
+++ data.py
@@ -1,9 +1,16 @@
-from datasets import load_dataset
+import os
+import json
+from PIL import Image
 from torchvision import transforms
-import torch
+from datasets import Dataset
 
 def load_and_preprocess_data(config):
-    dataset = load_dataset(config.dataset_name, split="train")
+    image_folder = os.path.join(config.data_dir, 'data') 
+    json_file = os.path.join(config.data_dir, 'config.json') 
+
+    # Загружаем описание из config.json
+    with open(json_file, 'r') as f:
+        descriptions = json.load(f)
 
     preprocess = transforms.Compose(
         [
@@ -15,8 +22,24 @@ def load_and_preprocess_data(config):
     )
 
     def transform(examples):
-        images = [preprocess(image.convert("RGB")) for image in examples["image"]]
-        return {"images": images}
+        images = []
+        texts = []
+
+        for image_name in examples['image_names']:
+            img_path = os.path.join(image_folder, image_name)
+            img = Image.open(img_path).convert("RGB")
+            img = preprocess(img)
+            
+            description = descriptions.get(image_name, "")
+            images.append(img)
+            texts.append(description)
+
+        return {"images": images, "descriptions": texts}
+
+    image_names = list(descriptions.keys())
+
+    dataset = Dataset.from_dict({"image_names": image_names})
 
     dataset.set_transform(transform)
+
     return dataset
